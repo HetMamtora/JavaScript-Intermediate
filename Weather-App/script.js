f2acb3c754070fc5e473a2e5d1985fc1ff8b57d3
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-14 14:29:07 +0530

    updated

---
+++ Weather-App/script.js
@@ -0,0 +1,17 @@
+const input = document.querySelector("input");
+const btn = document.getElementById("btn");
+const icon = document.querySelector("icon");
+const weather = document.querySelector("weather");
+const temperature = document.querySelector("temperature");
+const description = document.querySelector("description");
+
+const apikey='c976ae3183b09cc66a3c2888b27088e4';
+
+
+btn.add
+
+
+
+
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
