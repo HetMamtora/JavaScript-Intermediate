08dd044719b7725074400b6eeea655b6ac7eeafb
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-16 22:25:51 +0530

    updated

---
+++ Filter-Category/script.js
@@ -0,0 +1,10 @@
+const productList = document.getElementById("product-list");
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const categoryBtns = document.querySelectorAll(".category-btn");
+
+function filterProduct(){
+
+}
+
+filterProduct();
