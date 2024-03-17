8ec2ade45aa4d4255fc9c984d43d68787fe78254
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-17 16:06:34 +0530

    Update script.js

--- Filter-Category/script.js
+++ Filter-Category/script.js
@@ -4,7 +4,33 @@ const searchBtn = document.getElementById("search-btn");
 const categoryBtns = document.querySelectorAll(".category-btn");
 
 function filterProduct(){
+    const searchValue = searchInput.value.toLowerCase();
+    const productItems = document.querySelectorAll(".product-item");
 
+    productItems.forEach(item=>{
+        const title = item.querySelector("h3").innerText.toLowerCase();
+        const category = item.dataset.category;
+
+        if(title.includes(searchValue) || searchValue==""){
+            item.style.display="block";
+        }
+        else{
+            item.style.display="none";
+        }
+    })
 }
 
+function setCategory(e){
+    categoryBtns.forEach(btn=>{
+        e.target.classList.add("active");
+        filterProduct();
+    })
+}
+
+
+searchBtn.addEventListener("click", filterProduct);
+searchInput.addEventListener("keyup", filterProduct);
+categoryBtns.forEach(btn=>{
+    btn.addEventListener("click", setCategory);
+})
 filterProduct();
